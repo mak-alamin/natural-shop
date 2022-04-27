8f048b4bd7ec6fdd9511b3211ecf2acd734aa5d2
Mak Alamin <makalamin012@gmail.com>
2022-04-27 07:33:10 +0600

    dynamic location url

--- src/components/Admin/Header/Header.js
+++ src/components/Admin/Header/Header.js
@@ -7,10 +7,16 @@ const Header = () => {
         <div className="navbar-brand-wrapper d-flex justify-content-center">
           <div className="navbar-brand-inner-wrapper d-flex justify-content-between align-items-center w-100">
             <a className="navbar-brand brand-logo" href="index.html">
-              <img src="images/logo.png" alt="logo" />
+              <img
+                src={`${window.location.origin}/images/logo.png`}
+                alt="logo"
+              />
             </a>
             <a className="navbar-brand brand-logo-mini" href="index.html">
-              <img src="images/logo.png" alt="logo" />
+              <img
+                src={`${window.location.origin}/images/logo.png`}
+                alt="logo"
+              />
             </a>
             <button
               className="navbar-toggler navbar-toggler align-self-center"
