635410ee8f391ec3d49206bc5d8e73e5b34d7f1c
Mak Alamin <makalamin012@gmail.com>
2022-04-27 06:26:51 +0600

    added user form

--- src/components/Admin/AddNewUser/AddNewUser.js
+++ src/components/Admin/AddNewUser/AddNewUser.js
@@ -3,7 +3,58 @@ import React from "react";
 const AddNewUser = () => {
   return (
     <div>
-      <h2>Add New User</h2>
+      <div className="row">
+        <div className="col-md-6 grid-margin stretch-card">
+          <div className="card">
+            <div className="card-body">
+              <h4 className="card-title">Add New User</h4>
+              <form className="forms-sample">
+                <div className="form-group">
+                  <label for="exampleInputUsername1">Username</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="exampleInputUsername1"
+                    placeholder="Username"
+                  />
+                </div>
+                <div className="form-group">
+                  <label for="exampleInputEmail1">Email address</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    placeholder="Email"
+                  />
+                </div>
+                <div className="form-group">
+                  <label for="exampleInputPassword1">Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleInputPassword1"
+                    placeholder="Password"
+                  />
+                </div>
+                <div className="form-group">
+                  <label for="exampleInputConfirmPassword1">
+                    Confirm Password
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleInputConfirmPassword1"
+                    placeholder="Password"
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary mr-2">
+                  Add user
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,27 +1,30 @@
 import React from "react";
-
+import "./assets/css/style.css";
+import "./assets/vendors/base/vendor.bundle.base.css";
+import "./assets/vendors/datatables.net-bs4/dataTables.bootstrap4.css";
+import "./assets/vendors/mdi/css/materialdesignicons.min.css";
+import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import MainPanel from "./MainPanel/MainPanel";
 import Sidebar from "./Sidebar/Sidebar";
 
-import Footer from "./Footer/Footer";
-
 const Admin = () => {
   return (
-    <div className="container-scroller">
-      <Header></Header>
-      <div className="container-fluid page-body-wrapper">
-        <Sidebar></Sidebar>
+    <>
+      <div className="container-scroller">
+        <Header></Header>
+        <div className="container-fluid page-body-wrapper">
+          <Sidebar></Sidebar>
 
-        <div className="main-panel">
-          <div className="content-wrapper">
-            <MainPanel></MainPanel>
+          <div className="main-panel">
+            <div className="content-wrapper">
+              <MainPanel></MainPanel>
+            </div>
+            <Footer></Footer>
           </div>
-          <Footer></Footer>
         </div>
-        
       </div>
-    </div>
+    </>
   );
 };
 
